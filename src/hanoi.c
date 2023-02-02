05960773ad93a30ed0ee9f2b21c3ce2f5e487718
mooring <mooring@live.com>
2023-02-02 11:55:25 +0800

    add hanoi.c

---
+++ src/hanoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+void move(char from, char to){
+    printf("%c -> %c\n", from, to);
+}
+
+void hanoi(int m, char one, char two, char three){
+    if(m == 1){
+        move(one, three);
+    }else{
+        hanoi(m-1, one, three, two);
+        move(one, three);
+        hanoi(m-1, two, one, three);
+    }
+}
+
+int main(){
+    hanoi(4, 'A', 'B','C');
+    return 0;
+}
