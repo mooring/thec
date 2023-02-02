9c47b97e40616500b5b3e8f9611e90f17743eed3
mooring <mooring@live.com>
2023-02-02 17:44:42 +0800

    add array.c

---
+++ src/array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printCharArr(char *arr[], int asize){
+    int i = 0;
+    for(i=0; i<asize; i++){
+        printf("%2d %s\n", i+1, arr[i]);
+    }
+}
+
+int main(){
+    char* abc[] = {
+        {'A','B','C',0},
+        "abcdef",
+        {"XY"}
+    };
+    printCharArr(abc, sizeof(abc)/sizeof(char*));
+    return 0;
+}
